ebeeffecaaf2e4828f96511382635616059c10be
Gabriel Cantuária <gabriel13n@gmail.com>
2022-04-20 10:41:29 -0300

    exercicios map e filter

---
+++ Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercicio5.js
@@ -0,0 +1,6 @@
+function fantasyOrScienceFictionAuthors() {
+  const wantedGenres = ['Fantasia', 'Ficção Científica'];
+  return books
+    .filter((book) => wantedGenres.includes(book.genre))
+    .map((book) => book.author.name).sort();
+}
