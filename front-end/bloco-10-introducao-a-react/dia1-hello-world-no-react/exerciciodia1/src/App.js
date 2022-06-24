53cd744ff9c36b9bea46f692e0f8ba8c269167db
Gabriel Cantuária <gabriel13n@gmail.com>
2022-06-24 19:59:16 -0300

    exercicios dia 1

---
+++ front-end/bloco-10-introducao-a-react/dia1-hello-world-no-react/exerciciodia1/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
+
+const tarefas = ['Acordar', 'Tomar café', 'Escovar os dentes', 'Ir trabalhar']
+
+function App() {
+  return (
+    <ul>{tarefas.map((tarefa) => Task(tarefa))}</ul>
+  )
+  
+}
+
+export default App;
