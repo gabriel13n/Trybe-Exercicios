1112179501c093dfbb3431702500947e31280773
Gabriel Cantuária <gabriel13n@gmail.com>
2022-10-17 18:59:26 -0300

    renomeando

---
+++ Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objeto-e-funcoes/funcoes.js
@@ -0,0 +1,6 @@
+//preciso saber se uma palavra é um palindromo ou seja,
+//é lida de tras para frente da mesma forma
+//preciso percorrer a string e comparar sua ultima palavra com a primeira
+//se a primeira letra for diferente da ultima automaticamente não é
+//precisamos de uma condição
+function verificaPalindromo (string)
